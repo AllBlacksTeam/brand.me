49f13f34c93c537f6ee6fa2428e044f1e8a1c337
Salva <saruba@gmail.com>
2017-07-30 17:07:27 +0200

    server and client updates

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const expressGraphQL = require('express-graphql');
+
+const models = require('./models');
+const schema = require('./schema/schema');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
-
-
 
 // Set native promises as mongoose promise
 mongoose.Promise = global.Promise;
@@ -17,11 +20,14 @@ mongoose.connect(MONGO_URI, (error) => {
     console.error('Please make sure Mongodb is installed and running!'); // eslint-disable-line no-console
     throw error;
   }
+  console.log('Connected to MongoLab instance.');
 });
 
-// app.get('/', (req, res) => {
-//   res.send({ hello: 'world' });
-// });
+app.use(bodyParser.json());
+app.use('/graphql', expressGraphQL({
+  schema,
+  graphiql: true
+}));
 
 const webpackMiddleware = require('webpack-dev-middleware');
 const webpack = require('webpack');
