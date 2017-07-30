28d625d28d3c9f09eb069cf1e66e630bea9b0e74
Salva <saruba@gmail.com>
2017-07-30 12:16:54 +0200

    add base url

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+app.get('/', (req, res) => {
+  res.send({ hello: 'world' });
+})
+
 app.listen(PORT, (error) => {
   if (!error) {
     console.log(`Running on port: ${PORT}!`);
