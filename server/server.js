af00a2268359c00bde32ca365c8f7146ce701c3c
Salva <saruba@gmail.com>
2017-07-30 12:40:44 +0200

    client index

--- server/server.js
+++ server/server.js
@@ -1,10 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-  res.send({ hello: 'world' });
-})
+
+
+// Set native promises as mongoose promise
+mongoose.Promise = global.Promise;
+
+const DB_USER = 'antonio';
+const DB_PASSWORD = 'password';
+const MONGO_URI = `mongodb://${DB_USER}:${DB_PASSWORD}@ds129333.mlab.com:29333/brandme`;
+mongoose.connect(MONGO_URI, (error) => {
+  if (error) {
+    console.error('Please make sure Mongodb is installed and running!'); // eslint-disable-line no-console
+    throw error;
+  }
+});
+
+// app.get('/', (req, res) => {
+//   res.send({ hello: 'world' });
+// });
+
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpack = require('webpack');
+const webpackConfig = require('../webpack.config.js');
+app.use(webpackMiddleware(webpack(webpackConfig)));
 
 app.listen(PORT, (error) => {
   if (!error) {
